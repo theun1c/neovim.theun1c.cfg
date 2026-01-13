cae865d0a5c4ff2590ed5ee75f0e75cc3814fcbc
theun1c <belovdima1122@yandex.ru>
2026-01-13 12:39:14 +0300

    add lualine and mason

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,23 @@
+-- 1 pl
 vim.pack.add({
 	{src = "https://github.com/folke/tokyonight.nvim"},
 })
+
+require("tokyonight").setup({})
+
+-- 2 pl
+vim.pack.add({
+	{src = "https://github.com/mason-org/mason.nvim"},
+})
+
+require("mason").setup({})
+
+-- 3 pl
+vim.pack.add({
+	{src = "https://github.com/nvim-lualine/lualine.nvim"},
+})
+
+require("lualine").setup({})
+
+
+
