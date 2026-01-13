8b8a843ce8e4ed356f06a2b6bb45ee274d439bd4
theun1c <belovdima1122@yandex.ru>
2026-01-13 13:27:48 +0300

    add fzf plug

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,1 +1,7 @@
 vim.keymap.set("n", "<leader>e","<Cmd>Explore<CR>")
+
+local fzf = require("fzf-lua")
+vim.keymap.set("n", "<leader><leader>", fzf.files)
+vim.keymap.set("n", "<leader>/", fzf.live_grep)
+
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,5 +19,35 @@ vim.pack.add({
 
 require("lualine").setup({})
 
+-- 4 pl 
+vim.pack.add({
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+})
 
+local actions = require('fzf-lua.actions')
+require('fzf-lua').setup({
+	winopts = { backdrop = 85 },
+	keymap = {
+		builtin = {
+			["<C-f>"] = "preview-page-down",
+			["<C-b>"] = "preview-page-up",
+			["<C-p>"] = "toggle-preview",
+		},
+		fzf = {
+			["ctrl-a"] = "toggle-all",
+			["ctrl-t"] = "first",
+			["ctrl-g"] = "last",
+			["ctrl-d"] = "half-page-down",
+			["ctrl-u"] = "half-page-up",
+		}
+	},
+	actions = {
+		files = {
+			["ctrl-q"] = actions.file_sel_to_gf,
+			["ctrl-n"] = actions.toggle_ignore,
+			["ctrl-h"] = actions.toggle_hidden,
+			["enter"] = actions.file_edit_or_gf,
+		}
+	}
+})
 
